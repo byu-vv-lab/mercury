eae2da42dcd2b6f39e001e3293fbb4a01be831da
Joshua T. Asplund <joshasplund@gmail.com>
2015-09-18 15:52:46 -0600

    Tweaks order of tests to match (expected, actual) pattern, adds capture of stdout

--- src/test/java/Parser/ParserTest.java
+++ src/test/java/Parser/ParserTest.java
@@ -16,11 +16,11 @@ public class ParserTest {
     public void trivialPrograms () throws IOException {
         URL test1 = ParserTest.class.getResource("test1.jta");
         Program p = ProgramFactory.loadProgramFromFile(test1);
-        assertEquals(p, TestProgramFactory.test1());
+        assertEquals(TestProgramFactory.test1(), p);
 
         URL test2 = ParserTest.class.getResource("test2.jta");
         p = ProgramFactory.loadProgramFromFile(test2);
-        assertEquals(p, TestProgramFactory.test2());
+        assertEquals(TestProgramFactory.test2(), p);
     }
 
 }
